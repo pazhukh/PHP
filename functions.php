bc8896c6c303f42dbfabb861799d7b702bd0e4e5
pazhukh <pazhukh@gmail.com>
2018-01-03 12:02:10 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,7 @@
+//back button
+<?php
+			$url = htmlspecialchars($_SERVER['HTTP_REFERER']);
+?>
+<a href="<?php echo $url; ?>">Back</a>
+
+****************************************************************************************
