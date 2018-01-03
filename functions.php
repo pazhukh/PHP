81c35f0225a947c0234e09c8bc6edfbec947ef27
pazhukh <pazhukh@gmail.com>
2018-01-03 12:05:51 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 //back button
 <?php
-			$url = htmlspecialchars($_SERVER['HTTP_REFERER']);
+   $url = htmlspecialchars($_SERVER['HTTP_REFERER']);
 ?>
 <a href="<?php echo $url; ?>">Back</a>
+//JS
+<input type="button" value="Back" onclick="history.back(-1)" />
 
 ****************************************************************************************
